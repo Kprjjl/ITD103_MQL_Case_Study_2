7a63205e72e1389fad62f8560569de79f7e78c2e
Kprjjl <69335092+Kprjjl@users.noreply.github.com>
2024-05-21 13:24:26 +0800

    test components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import { 
+  Button, 
+  Typography, 
+  Card, 
+  CardHeader, 
+  CardBody 
+} from "@material-tailwind/react";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Card>
+        <CardBody>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            <Typography color="white">
+              count is {count}
+            </Typography>
+          </Button>
+          <Typography color="gray">
+            Edit <code>App.jsx</code> and save to test HMR
+          </Typography>
+        </CardBody>
+      </Card>
     </>
   )
 }
