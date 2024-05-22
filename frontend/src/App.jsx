d62ab221d1ef7b9ba28299160a6309cec454973e
Kprjjl <69335092+Kprjjl@users.noreply.github.com>
2024-05-23 01:31:25 +0800

    updated TrashCanIcon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,13 +13,14 @@ import TrashCanIcon from './components/TrashCanIcon';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [level, setLevel] = useState(50)
 
   return (
     <div className="min-h-screen bg-blue-gray-50/50 p-8">
       <Card>
         <CardBody>
         <div className='w-20 h-20 m-3 relative'>
-          <TrashCanIcon color="white" progressColor="black" progress={50}/>
+          <TrashCanIcon color="white" progressColor="lime" progress={level}/>
         </div>
           <Button onClick={() => setCount((count) => count + 1)}>
             <Typography color="white">
